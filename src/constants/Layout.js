c85a04542cfffd6fff2601cb176008cefc930da7
김동하 <kamdunga2002@gmail.com>
2021-07-18 18:19:07 +0900

    feat(constants/Layout): add Layout helper function

---
+++ src/constants/Layout.js
@@ -0,0 +1,39 @@
+import { Dimensions, Platform } from 'react-native';
+import SafeArea from 'react-native-safe-area';
+
+const WIDTH = Dimensions.get('window').width;
+const HEIGHT = Dimensions.get('window').height;
+
+const SCREEN_WIDTH = Dimensions.get('screen').width;
+const SCREEN_HEIGHT = Dimensions.get('screen').height;
+
+const Layout = {
+  window: {
+    width: WIDTH,
+    height: HEIGHT,
+  },
+
+  screen: {
+    width: SCREEN_WIDTH,
+    height: SCREEN_HEIGHT,
+  },
+
+  insets: {
+    right: 0,
+    top: 0,
+    left: 0,
+    bottom: 0,
+  },
+
+  responsive: number => (WIDTH / 375) * number,
+};
+
+export function initLayoutInsets() {
+  SafeArea.getSafeAreaInsetsForRootView().then(res => {
+    if (res && res.safeAreaInsets) {
+      Layout.insets = res.safeAreaInsets;
+    }
+  });
+}
+
+export default Layout;
