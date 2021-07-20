3c7b9557aa5a11cb51e00af48c9dc00df6f6642e
positiveko <edie_ko@naver.com>
2021-07-21 01:54:29 +0900

    feat: react-native-permission 추가

--- src/components/Camera/index.tsx
+++ src/components/Camera/index.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useEffect } from 'react';
+import { View, Alert, ToastAndroid } from 'react-native';
 import { RNCamera } from 'react-native-camera';
 import CameraRoll from '@react-native-community/cameraroll';
+import { PERMISSIONS, RESULTS, request } from 'react-native-permissions';
 import BottomTab from '@components/Camera/BottomTab';
 
 type TProps = {
@@ -11,17 +12,31 @@ type TProps = {
 export default function CameraIndex({ navigation }: TProps) {
   const cameraRef = React.useRef<any>();
 
+  useEffect(() => {
+    askPermission();
+  }, []);
+
+  const askPermission = async () => {
+    try {
+      const result = await request(PERMISSIONS.IOS.PHOTO_LIBRARY);
+      if (result === RESULTS.GRANTED) {
+      }
+    } catch (error) {
+      console.log('askPermission', error);
+    }
+  };
+
   const takePhoto = async () => {
-    console.log(cameraRef);
     if (cameraRef.current) {
       const data = await cameraRef.current.takePictureAsync({
         quality: 1,
         exif: true,
       });
-      console.log(data);
       if (data) {
-        const result = await CameraRoll.saveToCameraRoll(data.uri);
-        console.log(result);
+        console.log(data);
+        // const result = await CameraRoll.save(data.uri);
+        // console.log(result);
+        Alert.alert('Picture Taken!', data.uri);
       }
     }
   };
@@ -35,9 +50,16 @@ export default function CameraIndex({ navigation }: TProps) {
         backgroundColor: '#303030',
       }}>
       <RNCamera
+        ref={cameraRef}
         style={{ width: '100%', height: '100%' }}
-        type={RNCamera.Constants.Type.front}
+        type={RNCamera.Constants.Type.back}
         captureAudio={false}
+        androidCameraPermissionOptions={{
+          title: 'Permission to use camera',
+          message: 'We need your permission to use your camera',
+          buttonPositive: 'Ok',
+          buttonNegative: 'Cancel',
+        }}
       />
       <BottomTab navigation={navigation} takePhoto={takePhoto} />
     </View>
