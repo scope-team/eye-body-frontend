9f2f4c3c84f19bdedb3d57516124b3045feb643f
김동하 <kamdunga2002@gmail.com>
2021-07-20 20:51:20 +0900

    feat(Gallery/index): change local variable to parents component as state

--- src/components/Gallery/index.tsx
+++ src/components/Gallery/index.tsx
@@ -1,26 +1,51 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { View } from 'react-native';
 import Title from '@/components/Title';
 import PhotoList from '@/components/Gallery/PhotoList';
 import EditPhotoHeader from '@/components/Header/EditPhotoHeader';
 import useStackContext from '@/lib/context/useStackContext';
+import { MOCK_IMAGE_ARRAY } from '@/constants/gallery/mock';
+
+type TPhotos = {
+  src: string;
+  id: number;
+  isSelect: boolean;
+};
 
 type TProps = {
   navigation: any;
 };
 
 export default function GalleryIndex({ navigation }: TProps) {
+  const [photoList, setPhotoList] = useState<TPhotos[]>([]);
+  const [selectedPhotos, setSelectedPhotos] = useState<TPhotos[]>([]);
+
   const { GalleryStackType } = useStackContext();
   console.log(GalleryStackType);
+
+  useEffect(() => {
+    setPhotoList(MOCK_IMAGE_ARRAY);
+  }, []);
+
+  const selectedPhotoHandler = (id: number) => {
+    const newArray = photoList.map(el => {
+      return el.id === id ? { ...el, isSelect: !el.isSelect } : el;
+    });
+    setPhotoList(newArray);
+  };
+
   return (
     <View>
-      {GalleryStackType ? (
+      {!selectedPhotos.length ? (
         <Title title={GalleryStackType} navigation={navigation} />
       ) : (
-        <EditPhotoHeader />
+        <EditPhotoHeader selectedPhotos={selectedPhotos} />
       )}
-
-      <PhotoList navigation={navigation} />
+      <PhotoList
+        navigation={navigation}
+        selectedPhotoHandler={selectedPhotoHandler}
+        photoList={photoList}
+      />
     </View>
   );
 }
