7b474c147460151a50a6c0f6aff20f084921a44a
김동하 <kamdunga2002@gmail.com>
2021-07-25 20:50:33 +0900

    doc: add svg icon for effect page

--- src/components/Effect/EffectBottomMenu.tsx
+++ src/components/Effect/EffectBottomMenu.tsx
@@ -1,14 +1,46 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
 
 type Tprops = {
   effectName: string;
 };
 
 export default function EffectBottomMenu({ effectName }: Tprops) {
-  return (
-    <View style={{ marginTop: 40 }}>
-      <Text>{effectName}</Text>
-    </View>
-  );
+  const renderBottomMenu = () => {
+    switch (effectName) {
+      case 'Compare':
+        return (
+          <View>
+            <View style={{ flexDirection: 'row' }}>
+              <Text>텍스트 표시</Text>
+              <TouchableOpacity></TouchableOpacity>
+            </View>
+            <View>
+              <TouchableOpacity>
+                <Text>화이트</Text>
+              </TouchableOpacity>
+              <TouchableOpacity>
+                <Text>블랙</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        );
+      case 'Before & After':
+        return (
+          <View style={{ flexDirection: 'row' }}>
+            <TouchableOpacity>
+              <Text>+</Text>
+            </TouchableOpacity>
+            <View></View>
+            <TouchableOpacity>
+              <Text>-</Text>
+            </TouchableOpacity>
+          </View>
+        );
+
+      default:
+        return null;
+    }
+  };
+  return <View style={{ marginTop: 40 }}>{renderBottomMenu()}</View>;
 }
