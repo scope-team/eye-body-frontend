e7a1ca5b2408100c97abc337617416a055ef437b
positiveko <edie.spark1er@gmail.com>
2021-12-01 23:38:33 +0900

    set: rebuild settings

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,8 +1,8 @@
 module.exports = {
-  jsxBracketSameLine: true, // > 를 다음 줄로 내릴지 여부
-  singleQuote: true, //싱글쿼트 사용 여부
-  trailingComma: 'all', //여러 줄을 사용할 때, 후행 콤마
-  printWidth: 100, //줄바꿈할 너비
-  endOfLine: 'auto', //마지막 라인 엔터
-  arrowParens: 'avoid', //화살표 함수 괄호 사용 방식
+  jsxBracketSameLine: true,
+  singleQuote: true, 
+  trailingComma: 'all',
+  printWidth: 100,
+  endOfLine: 'auto', 
+  arrowParens: 'avoid',
 };
